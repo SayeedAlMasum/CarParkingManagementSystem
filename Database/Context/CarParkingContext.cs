36e8d7ef7265c124b13ffd0af6f50373c6442d86
SayeedAlMasum <githubms789@gmail.com>
2025-01-27 12:00:54 +0600

    Committed

--- Database/Context/CarParkingContext.cs
+++ Database/Context/CarParkingContext.cs
@@ -8,8 +8,8 @@ namespace Database.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-5NMO71P;Database=CarParkingManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
-            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-1H8PV8J\SQLEXPRESS01;Database=CarParkingManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
+            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-5NMO71P;Database=CarParkingManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-1H8PV8J\SQLEXPRESS01;Database=CarParkingManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
         }
         public DbSet<UserInfo> UserInfo { get; set; }
         public DbSet<Slot> Slot { get; set; }
